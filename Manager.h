bf5e0b2c09988274dda0c9de507bcf2dff170492
WebbyBubble <101310954+Webby-Bubble@users.noreply.github.com>
2023-07-05 20:41:46 +0800

    添加项目文件。

---
+++ Manager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"worker.h"
+#include<iostream>
+#include<string>
+class Manager :
+    public Worker
+{
+public:
+	Manager(int id,string name,int dId);
+	//显示个人信息
+	virtual void showInfo();
+	//获取岗位名称
+	virtual string getDeptName();
+};
+
